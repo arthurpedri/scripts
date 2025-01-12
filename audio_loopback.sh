aae70ca1b3ef19e97c46b56efb4a90bbd11859e4
Arthur Pedri Trevisol <arthur.pedri@gmail.com>
2025-01-12 02:05:39 -0300

    adds latency parameter and removes grep

--- audio_loopback.sh
+++ audio_loopback.sh
@@ -9,17 +9,20 @@ SOURCE_NAME="alsa_input.usb-Yamaha_Corporation_ARIUS-00.iec958-stereo"
 # pactl list sinks short
 SINK_NAME="alsa_output.usb-Generic_Blue_Microphones_2101BAB0FPH8-00.analog-stereo"
 
+# Under 40ms latency is too low
+LATENCY=${2:-"40"}
+
 # Check for the parameter
 if [ "$1" == "load" ]; then
-    # Load the loopback module with a latency parameter (modify as needed)
-    echo "Loopback module loading with source $SOURCE_NAME and sink $SINK_NAME."
-    pactl load-module module-loopback source=$SOURCE_NAME sink=$SINK_NAME latency_msec=40
+    # Load the loopback module with a latency parameter
+    echo "Loopback module loading with source $SOURCE_NAME and sink $SINK_NAME. Latency: $LATENCY ms"
+    pactl load-module module-loopback source=$SOURCE_NAME sink=$SINK_NAME latency_msec="$LATENCY"
 
 elif [ "$1" == "unload" ]; then
     # Unload the loopback module
-    MODULE_ID=$(pactl list modules short | grep 'module-loopback' | awk '{print $1}')
+    MODULE_ID=$(pactl list modules short | awk '/module-loopback/ {print $1}')
     if [ -n "$MODULE_ID" ]; then
-        pactl unload-module $MODULE_ID
+        pactl unload-module "$MODULE_ID"
         echo "Loopback module unloaded."
     else
         echo "No loopback module found to unload."
